fec91d08b90fd6c9983a67ecf60c8ce46dcb409e
Bruce Gordon <bruce@focusls.com>
2018-12-07 11:22:05 -0500

    Add project files.

---
+++ Service-api-mvc-client/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using EPiServer.Integration.Client.Models.Catalog;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Service_api_mvc_client.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Service_api_mvc_client.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HttpClient GetHttpClient()
+        {
+            var client = new HttpClient();
+            
+            return client;
+        }
+
+        public ActionResult Index()
+        {
+            Entries entries = null;
+            
+            return View(entries);
+        }
+
+        public ActionResult SingleEntry(string Code)
+        {
+            EntryViewModel viewModel = new EntryViewModel();
+
+            return View(viewModel);
+        }
+    }
+}
